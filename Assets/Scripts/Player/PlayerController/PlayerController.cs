a5d201f3b6d4e3ef0e4b6580fa4076ca4201abc6
kaneto <646374316@qq.com>
2019-10-14 18:54:24 +0800

    fea : 10.14
    - 完成左右走动以及跳跃的基本操作

--- Assets/Scripts/Player/PlayerController/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController/PlayerController.cs
@@ -5,17 +5,24 @@ public class PlayerController : MonoBehaviour {
     public GameInput Game_Input;
     [Header("Move")]
     public float MoveSpeed;
+    public float JumpForce;
     // move
     private Vector2 moveDir;
+    // jump
+    // private float jumpInteract;
+    private bool allowJump;
+    private float distance_toGround = 0.65f;
     private Rigidbody2D _rigidbody;
     private Transform _transform;
+
     private void Awake() {
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
-        _transform = transform;
+        _transform = gameObject.GetComponent<Transform>();
     }
     private void Update() {
         this.GetMoveDir();
         this.Move();
+        Jump();
     }
 
     private void GetMoveDir() {
@@ -25,5 +32,23 @@ public class PlayerController : MonoBehaviour {
         // _rigidbody.AddForce(moveDir * MoveSpeed * Time.deltaTime, ForceMode2D.Force);
         // _transform.Translate(moveDir * MoveSpeed * Time.deltaTime, Space.World);
         _rigidbody.velocity = new Vector2(moveDir.x * MoveSpeed, _rigidbody.velocity.y);
+    }
+    private void Jump() {
+        // jumpInteract = Game_Input.GetJumpInteraction();
+        // if (jumpInteract > 0) {
+        //     _rigidbody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        // }
+        OnGround();
+        if (Input.GetKeyDown(KeyCode.J) && allowJump) {
+            _rigidbody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+    private void OnGround() {
+        allowJump = Physics2D.Raycast(transform.position, Vector2.down, distance_toGround, 1 << LayerMask.NameToLayer("Ground"));
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(1, 0, 0);
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -distance_toGround, 0));
     }
 }
