72d5b027163f7c9cb1d57351727f3637db817fba
kaneto <646374316@qq.com>
2019-10-20 16:57:00 +0800

    fea : 10.20
    - 添加敌人复活的机制，方便测试

--- Assets/Scripts/AI/Enemy/EnemyController.cs
+++ Assets/Scripts/AI/Enemy/EnemyController.cs
@@ -7,7 +7,7 @@ public class EnemyController : MonoBehaviour
     public GameObject EnemyPrefab;
     public GameObject[] Enemies;
     public Vector2[] EnemiesPos;
-    private bool allowCreat;
+    private bool canCoroutine = true;
 
     private void Awake() {
         for (int i = 0; i < Enemies.Length; i++) {
@@ -15,11 +15,10 @@ public class EnemyController : MonoBehaviour
         }
     }
     private void Update() {
+        int i = 0;
         foreach (var enemy in Enemies) {
-            int i = 0;
-            if (enemy == null)
-                allowCreat = true;
-            if (allowCreat) {
+            if (enemy == null && canCoroutine) {
+                canCoroutine = false;
                 StartCoroutine(CreatNewEnemy(EnemyPrefab, EnemiesPos[i], i));
             }
             i++;
@@ -30,7 +29,6 @@ public class EnemyController : MonoBehaviour
     IEnumerator CreatNewEnemy(GameObject EnemyPrefab, Vector2 Pos, int index) {
         yield return new WaitForSeconds(5);
         Enemies[index] = Instantiate(EnemyPrefab, Pos, Quaternion.identity, gameObject.transform);
-        allowCreat = false;
-        StopCoroutine("CreatNewEnemy");
+        canCoroutine = true;
     }
 }
