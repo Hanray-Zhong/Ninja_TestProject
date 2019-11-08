a1113373c9f319cc9e0bd3b1b8a508888bfb4c44
kaneto <646374316@qq.com>
2019-11-09 01:48:14 +0800

    fix : 11.9
    - 稍微弄清楚了一点TileMap的用法

---
+++ Assets/Scripts/TileMap/TileMapCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "TileMapCube", menuName = "TileMapCube/Ground", order = 0)]
+public class TileMapCube : TileBase {
+    public Sprite TileSprite;
+    public Color TileColor;
+ 
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        tileData.sprite = TileSprite;
+        tileData.color = TileColor;
+        base.GetTileData(position, tilemap, ref tileData);
+    }
+    
+}
